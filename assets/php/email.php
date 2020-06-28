4cca5d4a2aada230eb7f80cdf30e524a3df9e013
yasiendwieb <yasien.dwieb@gmail.com>
2020-06-28 03:36:03 +0200

    add email sending logic

--- assets/php/email.php
+++ assets/php/email.php
@@ -1,55 +1,36 @@
 <?php
-mb_internal_encoding("UTF-8");
 
-$to = 'hello@example.com';
-$subject = 'Message from Cryptex';
+if($_POST) {
+    $visitor_name = "";
+    $visitor_email = "";
+    $visitor_message = "";
+    $email_title = "Bltzo Client Inquiry";
 
-$name = "";
-$email = "";
-$phone = "";
-$message = "";
+    if(isset($_POST['visitor_name'])) {
+        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
+    }
+    
+    if(isset($_POST['visitor_email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+    }
+        
+    if(isset($_POST['visitor_message'])) {
+        $visitor_message = htmlspecialchars($_POST['visitor_message']);
+    }
+    
+    $recipient = "info@bltzo.com";
+    
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
 
-if( isset($_POST['name']) ){
-    $name = $_POST['name'];
-
-    $body .= "Name: ";
-    $body .= $name;
-    $body .= "\n\n";
-}
-if( isset($_POST['subject']) ){
-    $subject = $_POST['subject'];
-}
-if( isset($_POST['email']) ){
-    $email = $_POST['email'];
-
-    $body .= "";
-    $body .= "Email: ";
-    $body .= $email;
-    $body .= "\n\n";
-}
-if( isset($_POST['phone']) ){
-    $phone = $_POST['phone'];
-
-    $body .= "";
-    $body .= "Phone: ";
-    $body .= $phone;
-    $body .= "\n\n";
-}
-if( isset($_POST['message']) ){
-    $message = $_POST['message'];
-
-    $body .= "";
-    $body .= "Message: ";
-    $body .= $message;
-    $body .= "\n\n";
-}
-
-$headers = 'From: ' .$email . "\r\n";
-
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-mb_send_mail($to, $subject, $body, $headers);
-    echo '<div class="status-icon valid"><i class="fa fa-check"></i></div>';
-}
-else{
+    if(mail($recipient, $email_title, $visitor_message, $headers)) {
+        echo '<div class="status-icon valid"><i class="fa fa-check"></i></div>';
+    }
+    else{
+        echo '<div class="status-icon invalid"><i class="fa fa-times"></i></div>';
+    }
+} else {
     echo '<div class="status-icon invalid"><i class="fa fa-times"></i></div>';
 }
